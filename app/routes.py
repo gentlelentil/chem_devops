e07f772b546ca383b7a1b416295ca99468e85662
Nathaniel Smith <nathaniel-smith@hotmail.co.uk>
2024-08-29 14:50:28 +0200

    updated layout

--- app/routes.py
+++ app/routes.py
@@ -117,7 +117,37 @@ def smiles_generator():
 
 #model url
 ADMET_MODEL_URL = 'http://ml_model:5001/predict'
-# ADMET_MODEL_URL = 'http://0.0.0.0:5001/predict'
+dev_ADMET_MODEL_URL = 'http://0.0.0.0:5001/predict'
+
+model_categories = {
+    'Absorption': {
+        'oral_abs_class': 'Oral Absorption',
+        'hia_class': 'Human Intestinal Absorption'
+    },
+    'Metabolism': {
+        'ML_input_p450-cyp3a4': 'CYP3A4 Inhibition',
+        'ML_input_p450-cyp2c19': 'CYP2C19 Inhibition',
+        'ML_input_p450-cyp1a2': 'CYP1A2 Inhibition',
+        'ML_input_p450-cyp2d6': 'CYP2D6 Inhibition',
+        'ML_input_p450-cyp2c9': 'CYP2C9 Inhibition'
+    },
+
+    'Distribution': {
+        'bbb_class': 'Blood Brain Barrier Permeability'
+    },
+    'Toxicity': {
+        'crl_toxicity_class': 'CRL Toxicity',
+        'hep_g2_toxicity_class': 'HepG2 Toxicity',
+        'hek_toxicity_class': 'HEK Toxicity',
+        'hacat_toxicity_class': 'HaCaT Toxicity',
+        'nih_toxicity_class': 'NIH Toxicity',
+        'ames_mutagenicity_class': 'Ames Mutagenicity'
+    },
+    'Cardiotoxicity': {
+        'herg_blockers_class': 'hERG Blockers'
+    },
+}
+
 
 #prediction route
 @auth.route('/admetpredictor', methods=['GET', 'POST'])
@@ -129,19 +159,33 @@ def admetpredictor():
         
         
         if smiles_string:
-            response = requests.post(ADMET_MODEL_URL, json={'smiles': smiles_string})
+            try:
+                response = requests.post(ADMET_MODEL_URL, json={'smiles': smiles_string})
+            except requests.exceptions.RequestException as e:
+                response = requests.post(dev_ADMET_MODEL_URL, json={'smiles': smiles_string})
+
             if response.status_code == 200:
                 result = response.json()
 
                 if 'prediction' in result:
+                    categorized_results = {category: [] for category in model_categories}
+
+                    for model_name, prediction in result['prediction'].items():
+                        for category, subcategories in model_categories.items():
+                            if model_name in subcategories:
+                                categorized_results[category].append((subcategories[model_name], prediction))
                    
-                    format_results = [(key, value) for key, value in result['prediction'].items()]
+                    # format_results = [(key, value) for key, value in result['prediction'].items()]
+
+
+
                 else:
                     format_results = 'No prediction available'
 
                 #admet_result because it is the FUNCTION NAME NOT THE HTML FILENAME
 
-                return render_template('admet-predictor.html', prediction=format_results, title='ADMET Predictor')
+                # return render_template('admet-predictor.html', prediction=format_results, title='ADMET Predictor')
+                return render_template('admet-predictor.html', prediction=categorized_results, title='ADMET Predictor')
 
                 # return redirect (url_for('auth.admet_result', result=result))
             else:
